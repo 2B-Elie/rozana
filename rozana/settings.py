5a1588408e90dcd781ec56324fbc6fcd9dece847
2B-Elie <baytabourkouelie@gmail.com>
2025-03-16 21:07:51 +0100

    Update sataticfiles

--- rozana/settings.py
+++ rozana/settings.py
@@ -147,10 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")  # Où les fichiers collectés seront stockés
+
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
