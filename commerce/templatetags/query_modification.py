63d6b983e419f4506d546e6e5d1791c5792aadb0
2B-Elie <baytabourkouelie@gmail.com>
2025-04-30 18:40:28 +0100

    Mise à jour de catégorie

--- commerce/templatetags/query_modification.py
+++ commerce/templatetags/query_modification.py
@@ -1,32 +1,22 @@
 from django import template
+from urllib.parse import urlencode
+from django.http import QueryDict
 
 register = template.Library()
 
-@register.simple_tag
-def modify_query(**kwargs):
-    # Cette fonction permet de modifier ou ajouter des paramètres à la query string
-    from django.http import QueryDict
-    from urllib.parse import urlencode
-    
-    request = kwargs.pop('request', None)
-    if request is None:
-        return ''
-    
+@register.simple_tag(takes_context=True)
+def modify_query(context, **kwargs):
+    request = context['request']
     query_dict = request.GET.copy()
     
     for key, value in kwargs.items():
         if value is None:
-            if key in query_dict:
-                del query_dict[key]
+            query_dict.pop(key, None)
         else:
             query_dict[key] = value
     
     # Gestion spéciale pour les catégories (multiples valeurs)
-    if 'category' in kwargs:
-        if kwargs['category'] is None:
-            query_dict.pop('category', None)
-        else:
-            query_dict.setlist('category', [str(kwargs['category'])])
+    if 'category' in kwargs and kwargs['category'] is None:
+        query_dict.pop('category', None)
     
     return query_dict.urlencode()
-
